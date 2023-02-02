ee26892edb2b906dc547d54f5310b322904777ab
veronicakolarska <veronica.kolarska@gmail.com>
2023-02-02 19:44:55 +0200

    fix: lint issue

--- TrainMe.DataBundler.WebAPI/Models/Common/EventBusConfiguration.cs
+++ TrainMe.DataBundler.WebAPI/Models/Common/EventBusConfiguration.cs
@@ -2,8 +2,8 @@ namespace TrainMe.DataBundler.Models.Common
 {
     public class EventBusConfiguration
     {
-        public string HostName { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string HostName { get; set; } = "";
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
     }
 }
